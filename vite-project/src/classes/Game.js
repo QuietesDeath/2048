dd270e96c02ea61fb505634664b812e186d93272
Quiet_Death <familuuuuudamira@gmail.com>
2025-10-22 22:38:50 +0300

    1
    1

---
+++ vite-project/src/classes/Game.js
@@ -0,0 +1,108 @@
+import Tile from "./Tile.js"
+
+export default class Game {
+
+	constructor(){
+		this.tileSize = 100
+		this.tileCount = 4
+		// пока не нужны
+
+		this.score = 0
+		this.bestScore = 0
+		
+		this.tiles =[
+			[null, null, null, null],
+			[null, null, null, null],
+			[null, null, null, null],
+			[null, null, null, null],
+		]
+	}
+
+	generateStyleSheet() {
+
+	 }
+
+	newGame() {
+		this.tiles = [
+			[null, null, null, null],
+			[null, null, null, null],
+			[null, null, null, null],
+			[null, null, null, null],
+		];
+		this.score = 0;
+		this.spawnTile();
+		this.spawnTile();
+	}
+
+	spawnTile(x, y, v) {
+		//let v = (Math.round(Math.random()) + 1) * 2
+		if (!x || !y) {
+			if (Math.random() > 0.75) {
+				v = 4;
+			} else {
+				v = 2;
+			}
+			let emptyCoords = this.findEmptySpaces();
+			let emptyPos = (randomEmptyPos =
+				emptyCoords[Math.floor(Math.random()) * emptyCoords.length]);
+			x = emptyPos.x;
+			y = emptyPos.y;
+			v = emptePos.v;
+		}
+		this.tiles[y][x] = new Tile(v);
+	}
+
+	findEmptySpaces() {
+		//
+		let emptyCoords = [];
+		for (let y = 0; y < this.tiles[y].length; y++) {
+			for (let x = 0; x < this.tiles[y].length; x++) {
+				if (!this.tiles[y][x]) emptyCoords.push({ x, y });
+			}
+		}
+		return emptyCoords;
+	}
+
+	moveLeft() {
+		for (let i = 0; y < this.tiles.length; y++) {
+			if (this.tiles[y][x]) {
+				this.tiles[y][0] = this.thiles[y][x]
+				this.tiles[y][x] = null
+			}
+		}
+	}
+	moveRight() {
+		for (let i = 0; y < this.tiles.length; y++) {
+			if (this.tiles[y][x]) {
+				this.tiles[y][x] = this.thiles[y][x]
+				this.tiles[y][0] = null
+			}
+		}
+	}
+	moveUp() {
+		for(let y = 0; y < this.tiles.length; y++) {
+			for(let x = 0; x < this.tiles[y].length; x++){
+				if(!this.tiles[y][x]) continue;
+				for(let o = 0; 0 < y; o++) {
+					if(!this.tiles[o][x]) {
+						this.tiles[o][x] = this.tiles[y][x];
+						this.tiles[y][x] = null
+					}
+				}
+			}
+		}
+	 }
+	moveDown() {
+		for(let x = 0; x < this.tiles.length; x++) {
+			for(let y = 0; y < this.tiles[y].length; y++){
+				if(!this.tiles[y][x]) continue;
+				for(let o = 0; 0 < x; o++) {
+					if(!this.tiles[y][o]) {
+						this.tiles[y][o] = this.tiles[y][x];
+						this.tiles[y][x] = null
+					}
+				}
+			}
+		}
+	}
+}
